60616202de2f21e58c63bea94be585a9f7b48e35
NaimaGulele <Naimagulele55@gmail.com>
2024-05-30 10:10:28 +0200

    code

--- src/App.jsx
+++ src/App.jsx
@@ -30,3 +30,5 @@ export const App = () => {
     </main>
   )
 }
+
+export default App;
--- src/components/Noteiem.jsx
+++ src/components/Noteiem.jsx
@@ -6,7 +6,7 @@ const Noteitem = ({ note }) => {
   return (
     <Link to={`/edit-note/${note.id}`}  className="note">
          <h4>{note.title.length > 50 ? note.title.slice (0, 50) + '...' : note.title}</h4>
-         <p>(note.date)</p> {/* Ensure date is used here */}
+         <p>{note.date}</p> {/* Ensure date is used here */}
 
     </Link>
   )
